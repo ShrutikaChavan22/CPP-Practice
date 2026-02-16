5fe7f0cf1806e7226bd0120b9c298e7e08e89ce9
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-16 19:07:32 +0530

    added area of circle

---
+++ Basics/AreaOfCircle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r,PI=3.14,area;
+    cout<<"Enter radius of circle:";
+    cin>>r;
+    area=PI*r*r;
+    cout<<"Area of circle:"<<area;
+    return 0;
+}
