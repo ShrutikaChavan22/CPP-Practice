9895637ea18b4ca066d617b07a97a4ac5783d902
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-16 19:24:48 +0530

    added Simple Interest program

---
+++ Basics/Simple_Interest.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float P,R,T;
+    cout<<"Enter the value of P,R,T:";
+    cin>>P>>R>>T;
+    float SI=P*R*T/100;
+    cout<<"Simple Intrest:"<<SI;
+}
