9fac4e8d700885b6e00362b8ce57cd0f5cdb50e8
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-17 09:38:09 +0530

    Added Square of number code

---
+++ Basics/SquareOfNo.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num,square;
+    cout<<"Enter any number:";
+    cin>>num;
+    square=num*num;
+    cout<<"Square of number:"<<square;
+    return 0;
+}
