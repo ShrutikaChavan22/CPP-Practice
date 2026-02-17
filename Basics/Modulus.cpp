52ea27b9d497ba88f8a9dd1d2459b9884dd58ae2
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-17 10:07:03 +0530

    added modulus code

---
+++ Basics/Modulus.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,sum;
+    cout<<"Enter value of first number:";
+    cin>>x;
+    cout<<"Enter value of second number:";
+    cin>>y;
+    cout<<"Remainder:"<<x%y;
+}
