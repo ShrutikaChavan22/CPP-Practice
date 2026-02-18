a1538ff6c643f16d7d7625f577f580789f77e050
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-18 08:56:18 +0530

    added ternary operator program

--- Conditionals/percentage.cpp
+++ Conditionals/percentage.cpp
@@ -5,13 +5,13 @@ int main(){
     int percentage;
     cout<<"Enter the percentage of student:";
     cin>>percentage;
-    if(percentage>81 and percentage<100){
+    if(percentage>=81 and percentage<=100){
         cout<<"Very good";
     }
-    else if(percentage>61 and percentage<80){
+    else if(percentage>=61 and percentage<=80){
         cout<<"good";
     }
-    else if(percentage>41 and percentage<60){
+    else if(percentage>=41 and percentage<-60){
         cout<<"average";
     }
     else
---
+++ Conditionals/ternary_operator.cpp
@@ -0,0 +1,10 @@
+//ternary operator
+#include<iostream>
+using namespace std;
+int main(){
+    int no;
+    cout<<"Enter any number:";
+    cin>>no;
+    (no%2==0)?cout<<"Even No":cout<<"Odd No";
+    return 0;
+}
