2fb6c6dfccc436c6b7c72aa423b68544e7cf272f
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-18 10:05:25 +0530

    Add loops folder

---
+++ Conditionals/forloop.cpp
@@ -0,0 +1,9 @@
+//print hello world 10 times using for loop
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<10;i++){
+        cout<<"Hello World!"<<endl;
+    }
+    return 0;
+}
