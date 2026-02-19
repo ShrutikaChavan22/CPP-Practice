667e41d5cd77f5907d0fa26697d8779a892ae5ab
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-19 21:56:10 +0530

    print odd number of triangle

--- Patterns/oddNoTriangle.cpp
+++ Patterns/oddNoTriangle.cpp
@@ -5,8 +5,8 @@ int main(){
     cout<<"Enter rows:";
     cin>>n;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=2*i-1;j+=2){
-            cout<<j<<" ";
+        for(int j=1;j<=i;j++){
+            cout<<2*j-1<<" ";
         }
         cout<<endl;
     }
