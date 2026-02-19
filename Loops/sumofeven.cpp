8334d1db03396ff646b81f95b5f2b16186026e7f
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-19 12:43:48 +0530

    calculate sum of only even number

---
+++ Loops/sumofeven.cpp
@@ -0,0 +1,17 @@
+//WAP to print sum of a even number.
+#include<iostream>
+using namespace std;
+int main(){
+    int no,sum=0,digit;
+    cout<<"Enter any number:";
+    cin>>no;
+    while(no!=0){
+        digit=no%10;
+        if(digit%2==0){
+        sum=sum+digit;
+        }
+        no=no/10;
+    }
+    cout<<"Sum of a even number:"<<sum;
+    return 0;
+}
