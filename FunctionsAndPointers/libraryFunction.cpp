f22a96785cde7a42e1140683a0a07512cca07bb6
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-20 23:02:15 +0530

    added library function

---
+++ FunctionsAndPointers/libraryFunction.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    cout<<"Square root of 16:"<<sqrt(16)<<endl;
+    cout<<"Cube root of 1000:"<<cbrt(1000)<<endl;
+    cout<<"Minimum from 1,6:"<<min(1,6)<<endl;
+    cout<<"Maximum from 10,100:"<<max(10,100)<<endl;
+    cout<<"Power of 2,3:"<<pow(2,3)<<endl;
+    return 0;
+}
