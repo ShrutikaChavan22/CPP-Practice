47a31607a8256df34b451be9ff71a4356d24ece9
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-20 17:10:06 +0530

    added star pyramid pattern

--- Patterns/star_pyramid.cpp
+++ Patterns/star_pyramid.cpp
@@ -6,17 +6,32 @@
 #include<iostream>
 using namespace std;
 int main(){
-   int m;
+   int n;
    cout<<"Enter number:";
-   cin>>m;
-    for(int i=1;i<=m;i++){
-       for(int j=1;j<=m-i;j++){
+   cin>>n;
+    /*for(int i=1;i<=n;i++){
+       for(int j=1;j<=n-i;j++){
           cout<<"  ";
        }
        for(int j=1;j<=2*i-1;j++){
           cout<<"* ";
        }
        cout<<endl;
-    }
+    }                                                          OR           */
+
+    //Method 2 -> nst and nsp
+   int nsp=n-1;
+   int nst=1;
+   for(int i=1;i<=n;i++){
+       for(int j=1;j<=nsp;j++){
+          cout<<"  ";
+       }
+       for(int j=1;j<=nst;j++){
+          cout<<"* ";
+       }
+       nsp--;
+       nst+=2;
+       cout<<endl;
+    }   
     return 0;
 }
