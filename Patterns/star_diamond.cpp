a8725346549235ed7674a2c74068b0da57a0b5ed
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-20 21:10:10 +0530

    added star diamond pattern

---
+++ Patterns/star_diamond.cpp
@@ -0,0 +1,52 @@
+/*print the given pattern
+       * 
+     * * *
+   * * * * *
+ * * * * * * *  */
+#include<iostream>
+using namespace std;
+int main(){
+   int n;
+   cout<<"Enter number:";
+   cin>>n;
+    /*for(int i=1;i<=n;i++){
+       for(int j=1;j<=n-i;j++){
+          cout<<"  ";
+       }
+       for(int j=1;j<=2*i-1;j++){
+          cout<<"* ";
+       }
+       cout<<endl;
+    }                                                          OR           */
+
+    //Method 2 -> nst and nsp
+   //upper half
+   int nsp=n-1;
+   int nst=1;
+   for(int i=1;i<=n;i++){
+       for(int j=1;j<=nsp;j++){
+          cout<<"  ";
+       }
+       for(int j=1;j<=nst;j++){
+          cout<<"* ";
+       }
+       nsp--;
+       nst+=2;
+       cout<<endl; 
+    }   
+    //lower half
+   nsp=1;
+   nst=2*n-3;
+   for(int i=1;i<=n;i++){
+       for(int j=1;j<=nsp;j++){
+          cout<<"  ";
+       }
+       for(int j=1;j<=nst;j++){
+          cout<<"* ";
+       }
+       nsp++;
+       nst-=2;
+       cout<<endl; 
+    }  
+    return 0;
+}
