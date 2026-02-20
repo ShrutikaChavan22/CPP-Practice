e95e2c3aadda1ccbdf6e3049e1bb877709f828a2
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-20 09:55:14 +0530

    print alphabet triangle vertically shift

---
+++ Patterns/star_alphabet_triangle_vertical.cpp
@@ -0,0 +1,22 @@
+/*print the given pattern 
+      A 
+    A B
+  A B C
+A B C D*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter any number";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<char(j+64)<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
