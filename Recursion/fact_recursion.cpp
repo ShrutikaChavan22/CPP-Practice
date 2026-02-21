ea8523575a6bb3c1b30473e45558e9b00ddc8bce
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-21 23:29:30 +0530

    calculate factorial using recursion

---
+++ Recursion/fact_recursion.cpp
@@ -0,0 +1,13 @@
+//Make a function which calculates the factorial of n using recursion
+#include<iostream>
+using namespace std;
+int fact(int n){
+    if(n==0 || n==1) return 1;
+    return n*fact(n-1);
+}
+int main(){
+    int n;
+    cout<<"Enter any no:";
+    cin>>n;
+    cout<<fact(n);
+}
