bdadc7d0e45ffb83bae66718683e0f0d96b6ad85
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-27 22:46:14 +0530

    create transpose of matrix

--- 2DArray/create_transpose.cpp
+++ 2DArray/create_transpose.cpp
@@ -1,1 +1,38 @@
 //write a program to create a  transpose and store it in new array.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int row, col;
+
+    cout << "Enter number of rows: ";
+    cin >> row;
+    cout << "Enter number of columns: ";
+    cin >> col;
+
+    int arr[row][col];
+    int transpose[col][row];
+
+    cout << "Enter elements of matrix:\n";
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+            cin >> arr[i][j];
+        }
+    }
+ 
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+            transpose[j][i] = arr[i][j];
+        }
+    }
+  
+    cout << "\nTranspose of matrix:\n";
+    for(int i = 0; i < col; i++) {
+        for(int j = 0; j < row; j++) {
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
