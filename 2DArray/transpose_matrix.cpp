7b3e5f175ee76ed9274d4da93073c3051180c406
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-27 22:52:08 +0530

    create transpose of matrix in that array

--- 2DArray/transpose_matrix.cpp
+++ 2DArray/transpose_matrix.cpp
@@ -1,19 +1,31 @@
 //Write a program to print the transpose of the matrix entered by the user.
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int a[2][3]={1,2,3,4,5,6};
-    for(int i=0;i<2;i++){
-        for(int j=0;j<3;j++){
-           cout<<a[i][j]<<" ";
-        }  
-        cout<<endl;
+
+int main() {
+    int row, col;
+
+    cout << "Enter number of rows: ";
+    cin >> row;
+    cout << "Enter number of columns: ";
+    cin >> col;
+
+    int arr[row][col];
+
+    cout << "Enter elements of the matrix:\n";
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+            cin >> arr[i][j];
+        }
     }
-    for(int i=0;i<3;i++){
-        for(int j=0;j<2;j++){
-           cout<<a[i][j]<<" ";
-        }  
-        cout<<endl;
+
+    cout << "\nTranspose of the matrix:\n";
+    for(int i = 0; i < col; i++) {
+        for(int j = 0; j < row; j++) {
+            cout << arr[j][i] << " ";
+        }
+        cout << endl;
     }
-        return 0;
+
+    return 0;
 }
